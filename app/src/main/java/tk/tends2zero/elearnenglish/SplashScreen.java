b8d7bdd0ecb9a220695a2ac097cb16f7bf97cb23
4tik <atik.csevu@gmail.com>
2018-10-20 19:34:27 -0700

    Splash Screen Add

--- app/src/main/java/tk/tends2zero/elearnenglish/SplashScreen.java
+++ app/src/main/java/tk/tends2zero/elearnenglish/SplashScreen.java
@@ -3,6 +3,9 @@ package tk.tends2zero.elearnenglish;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.analytics.connector.AnalyticsConnector;
 
 import static android.os.SystemClock.sleep;
 
@@ -12,9 +15,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        sleep(2000);
-        Intent intent= new Intent(this,SignIn.class);
-        startActivity(intent);
-        finish();
+        
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, SignIn.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2000);
+        //sleep(2000);
+        //Intent intent= new Intent(this,SignIn.class);
+        //startActivity(intent);
+        //finish();
     }
 }
